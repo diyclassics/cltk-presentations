62435982836511219fe8136d25d1b2e2f6be2cfb
Patrick J. Burns <patricks@diyclassics.org>
2019-01-10 10:10:40 -0800

    Load additional model

--- install-corpora.py
+++ install-corpora.py
@@ -1,3 +1,4 @@
 from cltk.corpus.utils.importer import CorpusImporter
 corpus_importer = CorpusImporter('latin')
 corpus_importer.import_corpus('latin_text_latin_library')
+corpus_importer.import_corpus('latin_models_cltk')
