8b562d18f6094f4ed9dfdc4404ddeb5f5f130037
Patrick J. Burns <patricks@diyclassics.org>
2019-01-11 11:52:55 -0800

    Fix build script

--- install-corpora.py
+++ install-corpora.py
@@ -2,4 +2,5 @@ from cltk.corpus.utils.importer import CorpusImporter
 corpus_importer = CorpusImporter('latin')
 corpus_importer.import_corpus('latin_text_latin_library')
 corpus_importer.import_corpus('latin_models_cltk')
+corpus_importer = CorpusImporter('greek')
 corpus_importer.import_corpus('greek_models_cltk')
